1a9a079a423863501414786bdcc20f5fc3930fea
Stephan Bischoff <bischoff.stephan@scheidt-bachmann.de>
2020-09-11 14:28:31 +0200

    Refactoring

---
+++ audiofiles.js
@@ -0,0 +1,15 @@
+const audioFiles = [
+    'audio/beginne-die-uebung-in.mp3',
+    'audio/beende-die-uebeung-in.mp3',
+    'audio/noch-10-sekunden.mp3',
+    'audio/noch-20-sekunden.mp3',
+    'audio/noch-30-sekunden.mp3',
+    'audio/noch-40-sekunden.mp3',
+    'audio/und-los.mp3',
+    'audio/wechsle-die-seite-und-beginne-die-uebung-in.mp3',
+    'audio/zeit-fuer-eine-kurze-pause.mp3',
+    'audio/mache-dich-bereit-fuer-die-naechste-uebeungt.mp3',
+    'audio/drei.mp3',
+    'audio/zwei.mp3',
+    'audio/eins.mp3'];
+
---
+++ exercises.js
@@ -0,0 +1,103 @@
+const exercises = [
+    {
+        title: "Supermann",
+        sides: 1
+    },
+    {
+        title: "Käfer auf dem Bauch",
+        sides: 2
+    },
+    {
+        title: "Käfer auf dem Rücken",
+        sides: 1
+    },
+    {
+        title: "Schmetterling",
+        sides: 1
+    },
+    {
+        title: "Ausfallschritt, nach vorne und halten",
+        sides: 2
+    },
+    {
+        title: "Ausfallschritt, zur Seite mit rotierenden Armen",
+        sides: 2
+    },
+    {
+        title: "Kniebeugen",
+        sides: 1
+    },
+    {
+        title: "Liegestütz, Oberkörper auf- und ab",
+        sides: 1
+    },
+    {
+        title: "Liegestütz, mit kleinen Schritten vor- und zurück",
+        sides: 1
+    },
+    {
+        title: "Liegestütz, abwechselnd die Knie anziehen",
+        sides: 1
+    },
+    {
+        title: "Brücke",
+        sides: 1
+    },
+    {
+        title: "Seitstütz, auf Unterarm",
+        sides: 2
+    },
+    {
+        title: "Sit-ups",
+        sides: 1
+    },
+    {
+        title: "Sit-ups seitlich",
+        sides: 1
+    },
+    {
+        title: "Sitzen, Beine abwechselnd ausstrecken",
+        sides: 1
+    },
+    {
+        title: "Gesäß auf Fersen",
+        sides: 1
+    },
+    {
+        title: "Rückenlage, Gesäß abheben und ein Bein anziehen",
+        sides: 2
+    },
+    {
+        title: "Rückenlage, Gesäß abheben und ein Bein ausstrecken",
+        sides: 2
+    },
+    {
+        title: "Seitenlage, Beine abheben und halten",
+        sides: 2
+    },
+    {
+        title: "Seitenlage, Beine abheben und oberes Bein auf und ab",
+        sides: 2
+    },
+    {
+        title: "Seitenlage, Beine abheben und Arme strecken",
+        sides: 2
+    },
+    {
+        title: "Vierfüßlerstand, Arm und Bein diagonal strecken",
+        sides: 1
+    },
+    {
+        title: "Vierfüßlerstand, Arm nach oben rotieren",
+        sides: 2
+    },
+    {
+        title: "Vierfüßlerstand, Knie anheben",
+        sides: 1
+    },
+    {
+        title: "Brücke",
+        sides: 1
+    },
+];
+
