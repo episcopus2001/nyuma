4767a4fc01d61d6d6551efa0cacebf281f786a21
Stephan Bischoff <bischoff.stephan@scheidt-bachmann.de>
2020-09-13 12:17:52 +0200

    Fixes

--- audiofiles.js
+++ audiofiles.js
@@ -8,7 +8,8 @@ const audioFiles = [
     'audio/und-los.mp3',
     'audio/wechsle-die-seite-und-beginne-die-uebung-in.mp3',
     'audio/zeit-fuer-eine-kurze-pause.mp3',
-    'audio/mache-dich-bereit-fuer-die-naechste-uebeungt.mp3',
+    'audio/mach-dich-bereit-fuer-die-naechste-uebung.mp3',
+    'audio/mach-dich-bereit-fuer-die-erste-uebung.mp3',
     'audio/drei.mp3',
     'audio/zwei.mp3',
     'audio/eins.mp3'];
--- exercises.js
+++ exercises.js
@@ -1,103 +1,153 @@
 const exercises = [
     {
-        title: "Supermann",
-        sides: 1
+        id: "supermannn",
+        title: "Bauchlage, Arme, Beine und Oberkörper anheben (Supermann)",
+        sides: 1,
     },
     {
-        title: "Käfer auf dem Bauch",
-        sides: 2
+        id: "ausfallschritt",
+        title: "Ausfallschritt, hinteres Knie über dem Boden halten",
+        sides: 2,
     },
     {
-        title: "Käfer auf dem Rücken",
-        sides: 1
+        id: "seitschritt-1",
+        title: "Seitschritt mit rotierenden Armen",
+        sides: 2,
     },
     {
-        title: "Schmetterling",
-        sides: 1
+        id: "seitschritt-2",
+        title: "Seitschritt, Hände vor der Brust zusammengepresst",
+        sides: 2,
     },
     {
-        title: "Ausfallschritt, nach vorne und halten",
-        sides: 2
-    },
-    {
-        title: "Ausfallschritt, zur Seite mit rotierenden Armen",
-        sides: 2
-    },
-    {
+        id: "kniebeugen",
         title: "Kniebeugen",
-        sides: 1
+        sides: 1,
     },
     {
-        title: "Liegestütz, Oberkörper auf- und ab",
-        sides: 1
+        id: "liegestuetz-1",
+        title: "Liegestütz",
+        sides: 1,
     },
     {
-        title: "Liegestütz, mit kleinen Schritten vor- und zurück",
-        sides: 1
+        id: "liegestuetz-2",
+        title: "Liegestütz und mit kleinen Schritten vor- und zurück",
+        sides: 1,
     },
     {
-        title: "Liegestütz, abwechselnd die Knie anziehen",
-        sides: 1
+        id: "liegestuetz-3",
+        title: "Liegestütz und abwechselnd die Knie zum Ellenbogen ziehen",
+        sides: 1,
     },
     {
-        title: "Brücke",
-        sides: 1
+        id: "seitstuetz-1",
+        title: "Seitstütz",
+        sides: 2,
     },
     {
-        title: "Seitstütz, auf Unterarm",
-        sides: 2
+        id: "seitstuetz-2",
+        title: "Seitstütz und ein Bein abheben",
+        sides: 2,
     },
     {
+        id: "unterarmstuetz",
+        title: "Unterarmstütz",
+        sides: 1,
+    },
+    {
+        id: "situps-1",
         title: "Sit-ups",
-        sides: 1
+        sides: 1,
     },
     {
-        title: "Sit-ups seitlich",
-        sides: 1
+        id: "situps-2",
+        title: "Sit-ups, seitlich",
+        sides: 1,
     },
     {
+        id: "sitz-1",
         title: "Sitzen, Beine abwechselnd ausstrecken",
-        sides: 1
+        sides: 1,
     },
     {
+        id: "fersen",
         title: "Gesäß auf Fersen",
-        sides: 1
+        sides: 1,
     },
     {
-        title: "Rückenlage, Gesäß abheben und ein Bein anziehen",
-        sides: 2
-    },
-    {
-        title: "Rückenlage, Gesäß abheben und ein Bein ausstrecken",
-        sides: 2
-    },
-    {
+        id: "seitenlage-1",
         title: "Seitenlage, Beine abheben und halten",
-        sides: 2
+        sides: 2,
     },
     {
+        id: "seitenlage-2",
         title: "Seitenlage, Beine abheben und oberes Bein auf und ab",
-        sides: 2
+        sides: 2,
     },
     {
+        id: "seitenlage-3",
         title: "Seitenlage, Beine abheben und Arme strecken",
-        sides: 2
+        sides: 2,
     },
     {
-        title: "Vierfüßlerstand, Arm und Bein diagonal strecken",
-        sides: 1
+        id: "vierfuessler-1",
+        title: "Vierfüßlerstand, Arm und Bein diagonal zusammenführen und strecken",
+        sides: 1,
     },
     {
-        title: "Vierfüßlerstand, Arm nach oben rotieren",
-        sides: 2
+        id: "vierfuessler-2",
+        title: "Vierfüßlerstand und ein Arm nach oben rotieren",
+        sides: 2,
     },
     {
-        title: "Vierfüßlerstand, Knie anheben",
-        sides: 1
+        id: "vierfuessler-3",
+        title: "Vierfüßlerstand und beide Knie anheben",
+        sides: 1,
     },
     {
-        title: "Brücke",
-        sides: 1
+        id: "schulterbruecke-1",
+        title: "Schulterbrücke und ein Bein anheben",
+        sides: 2,
+    },
+    {
+        id: "schulterbruecke-2",
+        title: "Schulterbrücke und ein Bein anwinkeln",
+        sides: 2,
+    },
+    {
+        id: "schulterbruecke-3",
+        title: "Schulterbrücke, ein Bein anheben und Arme nach oben strecken",
+        sides: 2,
+    },
+    {
+        id: "beine-kippen-1",
+        title: "Rückenlage, beide Beine anstellen und abwechselnd zur Seite kippen",
+        sides: 1,
+    },
+    {
+        id: "beine-kippen-2",
+        title: "Rückenlage, beide Beine anwinkeln und abwechselnd zur Seite kippen",
+        sides: 1,
+    },
+    {
+        id: "ruecken-situps",
+        title: "Rückenlage, ein Bein strecken, Knie des anderen Beins und Ellenbogen zusammenführen",
+        sides: 1,
+    },
+    {
+        id: "ruecken-bein-anziehen",
+        title: "Rückenlage, ein Bein anwinkeln und mit den Händen zur Brust ziehen",
+        sides: 2,
+    },
+    {
+        id: "tiefe-hocke",
+        title: "Tiefe Hocke",
+        sides: 1,
+    },
+    {
+        id: "29",
+        title: "Katzenbuckel",
+        sides: 1,
     },
 ];
 
